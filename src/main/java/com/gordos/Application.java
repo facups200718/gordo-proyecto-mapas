8640707e693704f5e3df32a0b2077525a9412b81
Cristian <cristianoreste39@gmail.com>
2023-05-12 11:04:45 -0300

    added app.yml

--- src/main/java/com/gordos/Application.java
+++ src/main/java/com/gordos/Application.java
@@ -1,14 +1,7 @@
 package com.gordos;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
